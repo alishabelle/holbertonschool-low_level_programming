c99c73035a99a07e3acce9c937dfd66cc03f40e7
alishabelle <alishabelle98@gmail.com>
2019-02-07 00:25:24 +0000

    print various size types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,11 @@ void main()
 {                                                       
 	int i;
 	clrscr();
-	printf(“Size of a char: %2d bytes \n", sizeof(char));
-        printf(“Size of an int:  %2d bytes \n", sizeof(int));
+	printf(“Size of a char: %2d bytes\n", sizeof(char));
+        printf(“Size of an int: %2d bytes\n", sizeof(int));
 	printf(“Size of a long int: %2d bytes \n", sizeof(long int));
         printf(“Size of a long long int * : %2d bytes \n", sizeof(long int *));
 	printf(“Size of a float: %2d bytes \n", sizeof(float));
 	getch();
+        return (0);
 }
