5b336eed253d2d632c2b65a328be4ccd303917a1
alishabelle <alishabelle98@gmail.com>
2019-02-07 00:18:23 +0000

    print various size types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+
+/**
+*main - The main function
+*Return: The return value is zero
+**/
+      
+void main()
+{                                                       
+	int i;
+	clrscr();
+	printf(“Size of a char: %2d bytes \n", sizeof(char));
+        printf(“Size of an int:  %2d bytes \n", sizeof(int));
+	printf(“Size of a long int: %2d bytes \n", sizeof(long int));
+        printf(“Size of a long long int * : %2d bytes \n", sizeof(long int *));
+	printf(“Size of a float: %2d bytes \n", sizeof(float));
+	getch();
+}
