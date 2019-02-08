6a6666855e976ec1ece4b94c705e732766366255
alishabelle <alishabelle98@gmail.com>
2019-02-08 20:52:37 +0000

    print single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - print all single digits
+*Return: single digits
+**/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x);
+	}
+	return (0);
+}
