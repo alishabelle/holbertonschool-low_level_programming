f899f1a6fa3b4f23c9b0be0c3a38b911e83e18ac
alishabelle <alishabelle98@gmail.com>
2019-02-08 22:44:53 +0000

    prints #'s 00-99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main - alphabet except q and e
+*Return: The return is the alphabet
+**/
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			putchar(x);
+			putchar(y);
+			if (x < '9' || y < '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
