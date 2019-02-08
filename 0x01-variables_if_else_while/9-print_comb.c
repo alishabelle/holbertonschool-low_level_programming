1eb41ea926cb68797a5fe760fb48b20f377265bb
alishabelle <alishabelle98@gmail.com>
2019-02-08 22:12:21 +0000

    single digit combos

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,8 +11,12 @@ int main(void)
 	for (x = '0'; x <= '9'; x++)
 	{
 		putchar(x);
-		putchar(',');
-		putchar(' ');
+		if (x != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
+	putchar('\n');
 	return (0);
 }
