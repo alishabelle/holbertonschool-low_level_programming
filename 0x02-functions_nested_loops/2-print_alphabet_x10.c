d5b0beed764a4cf7f72af11d02ae31701c2e92c5
alishabelle <alishabelle98@gmail.com>
2019-02-12 19:25:55 +0000

    printing a loop multiple times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+*print_alphabet_x10 - prints out alphabet
+*Return: The return is the alphabet 10x
+**/
+
+void print_alphabet_x10(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('\n');
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+	}
+}
