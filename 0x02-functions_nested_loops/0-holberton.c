51805a6e21cdc104eedef6542d8be5e3800e575e
alishabelle <alishabelle98@gmail.com>
2019-02-12 18:15:21 +0000

    print out chars

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,10 +7,15 @@
 
 int main(void)
 {
-
-	for (i = 0; i <= 9; i++)
-	{
-		_putchar(c[i]);
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
