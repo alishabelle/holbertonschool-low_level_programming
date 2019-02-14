3d42a8d43d7aba6c9255fb6a0ac977d038b700d1
alishabelle <alishabelle98@gmail.com>
2019-02-14 03:18:58 +0000

    print 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+*times_table - The main function
+*Return: The return value is zero
+**/
+void times_table(void)
+{
+	int z, i, x;
+
+	for (z = 0; z <= 9; z++)
+	{
+		_putchar('0');
+		for (i = 0; i <= 9; i++)
+		{
+			if (x <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(x + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+			}
+		}
+	x++;
+	_putchar('\n');
+	}
+}
