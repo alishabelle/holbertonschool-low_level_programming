841cd65adcc183f2321171f0d6dc95086fd0b032
alishabelle <alishabelle98@gmail.com>
2019-02-14 19:36:36 +0000

    checkers

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+*mul - The main function
+*@a: parameter
+*@b: parameter
+*Return: The return value is zero
+**/
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,5 @@
+#include "holberton.h"
+/**
+ *main - The main function
+ * Return: The return value is zero
+ **/
