a35597f407157190ee1e21aeca537b3d49ce7c8b
alishabelle <alishabelle98@gmail.com>
2019-02-14 16:08:10 +0000

    function checks for digit

--- 0x03-more_functions_nested_loops/1-isdigit.c
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
-*main - The main function
+*_isdigit - The main function
+*@c: parameter
 *Return: The return value is zero
 **/
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-	{		
+	{
 		return (1);
 	}
 	return (0);
