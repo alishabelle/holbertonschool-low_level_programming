c142f57c828e7e421d409ec558ec78ab774d7df1
alishabelle <alishabelle98@gmail.com>
2019-02-15 06:27:27 +0000

    drawing lines in the terminal

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -6,8 +6,14 @@
 **/
 void print_line(int n)
 {
-	if (n < 0)
-		_putchar('\n');
-	else
-		_putchar('_');
+	int x;
+
+	if (n > 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
 }
