4137f3f7ff5ecf6ac5c1e659a74f016298143dc4
alishabelle <alishabelle98@gmail.com>
2019-02-15 01:12:57 +0000

    print numbers 0-9

--- 0x03-more_functions_nested_loops/3-print_numbers.c
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
 /**
-*main - The main function
+*print_numbers - The main function
 * Return: The return value is zero
 **/
 void print_numbers(void)
 {
 	int x;
+
 	for (x = 0; x <= 9; x++)
-	{	_putchar(x + '0');
-       	}
+	{
+		_putchar(x + '0');
+	}
 	_putchar('\n');
 }
