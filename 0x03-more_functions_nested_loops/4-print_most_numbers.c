ef3eacf1b93b49f22f05ba598085d1b074ee94a1
alishabelle <alishabelle98@gmail.com>
2019-02-15 01:24:46 +0000

    print numbers except 2/4

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+*print_most_numbers - The main function
+* Return: The return value is zero
+**/
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x != 2 && x != 4)
+		{
+			_putchar(x + '0');
+		}
+	}
+	_putchar('\n');
+}
