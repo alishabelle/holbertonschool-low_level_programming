07b100f8c13c37a34f818ac0104158bcf97f0ef0
Trill Ali <alishabelle98@gmail.com>
2019-02-19 21:46:16 +0000

    print squares

--- 0x03-more_functions_nested_loops/8-print_square.c
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -7,15 +7,15 @@ void print_square(int size)
 {
 	int x, y;
 
-	if (size <= 0)
+	if (size < 0)
 		_putchar('\n');
 	else
 	{
 		for (x = 0; x < size; x++)
 		{
-			_putchar('\n');
 			for (y = 0; y < size; y++)
 				_putchar('#');
+			_putchar('\n');
 		}
 	}
 }
