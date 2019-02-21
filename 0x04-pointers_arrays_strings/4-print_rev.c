dd9fdec0ea4ad493ed1c3f23c617d29effedf9a3
alishabelle <alishabelle98@gmail.com>
2019-02-21 18:02:59 +0000

    print string in reverse

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *
+ *
+ */
+void print_rev(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != 0; x++)
+		;
+	for (x--; x >= 0; x--)
+		_putchar(s[x]);
+	_putchar('\n');
+
+
+}
