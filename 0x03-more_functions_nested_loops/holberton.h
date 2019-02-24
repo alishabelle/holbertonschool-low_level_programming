03582886e7a07d1d5b842185c94931d561ae00a2
Alisha <alishabelle98@gmail.com>
2019-02-24 21:53:46 +0000

    push header

--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -1,3 +1,6 @@
+#ifndef JANKY
+#define JANKY
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,3 +11,6 @@ void print_line(int n);
 int _putchar(char c);
 void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
+
+#endif
