90c5ca4cfd20ad5304d55af6fcf7a7df1288a9d1
Alisha <alishabelle98@gmail.com>
2019-02-24 20:01:25 +0000

    printing triangle backwards

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *
+ */
+void print_triangle(int size)
+{
+	int space, x;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (space = 0; space < size; space++)
+		{
+			for (x = space + 1; x < size; x++)
+				_putchar(' ');
+			for (x = 0; x <= space; x++)
+				_putchar('#');
+      			_putchar('\n');
+		}
+	}
+
+}
