3ff4851fb473833d8148fe70e270d1a63acb8e01
Trill Ali <alishabelle98@gmail.com>
2019-02-25 04:26:29 +0000

    prints half strings

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -2,16 +2,17 @@
 /**
  *puts_half - the main function
  *@str: parameter
+ * return - the return is void
  */
 void puts_half(char *str)
 {
 	int x, n;
-	n = (str[ ] - 1) / 2;
 
 	for (x = 0; str[x] != 0; x++)
-		if ( str[x] % 2 != 0)
-		{
-			_putchar(str[n] + '0');
-		}
+		;
+	for (n = x / 2; str[n] != 0; n++)
+	{
+		_putchar(str[n]);
+	}
 	_putchar('\n');
 }
