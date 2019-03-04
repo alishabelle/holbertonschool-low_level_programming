6834e27cd885f0b78fc970add6dba9b3214a8df5
Trill Ali <alishabelle98@gmail.com>
2019-03-04 20:10:09 +0000

    print array of elements

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -12,9 +12,10 @@ void print_array(int *a, int n)
 
 	for (x = 0; x < n; x++)
 	{
-		printf("%d, ", a[x]);
-		if (x > n)
-			printf("%d", a[x]);
-	}
-	printf("\n");
+		printf("%d",  a[x]);
+		if (x < (n - 1))
+			printf(", ");
+		else
+			printf("\n");
+ 	}
 }
