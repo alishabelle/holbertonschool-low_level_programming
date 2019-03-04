5e3a375eedd97f1766c1865782174910533b63a1
Trill Ali <alishabelle98@gmail.com>
2019-03-04 14:42:42 +0000

    put half a string

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -9,11 +9,10 @@ void puts_half(char *str)
 {
 	int x, n;
 
-	for (x = 1; str[x] != 0; x++)
+	for (x = 0; str[x] != 0; x++)
 		;
-	if (x % 2 != 0)
 		x--;
-	for (n = x / 2; str[n] != 0; n++)
+		for (n =(x / 2) + 1; str[n] != 0; n++)
 	{
 		_putchar(str[n]);
 	}
