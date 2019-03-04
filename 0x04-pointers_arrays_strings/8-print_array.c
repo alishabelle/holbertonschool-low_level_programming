634ca40a53826cf564edd4a2d41008f91c2d6834
Trill Ali <alishabelle98@gmail.com>
2019-03-04 19:20:30 +0000

    print out array elements

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,20 @@
+#include <stdio.h>
 #include "holberton.h"
 /**
- *
- *
+ *print_array - the main function
+ *@a: parameter
+ *@n: parameter
+ * Return: the return is void
  */
 void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%d, ", a[x]);
+		if (x > n)
+			printf("%d", a[x]);
+	}
+	printf("\n");
+}
