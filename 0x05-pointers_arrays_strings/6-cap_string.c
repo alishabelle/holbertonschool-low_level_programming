bf78e9cb2a8687c861275aeea2a0a5bcbe4c280e
Trill Ali <alishabelle98@gmail.com>
2019-03-07 18:48:37 +0000

    capitalize words of a string

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *cap_string - the main function
+ *@*: parameter
+ */
+char *cap_string(char *s)
+{
+	int x, y;
+	char arr[] = " \t\n,;.!?\"(){}";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if(s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] = s[0] - 32;
+		}
+		for (y = 0; arr[y] != '\0'; y++)
+		{
+			if (s[x] == arr[y] && s[x + 1] >= 97 && s[x + 1] <= 122)
+			{
+				s[x + 1] = s[x + 1] - 32;
+			}
+		}
+	}
+	return (s);
+}
