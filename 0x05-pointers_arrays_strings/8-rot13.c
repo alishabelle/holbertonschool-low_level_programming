5e12b06b55d09209cdde754916699279c0616999
Trill Ali <alishabelle98@gmail.com>
2019-03-08 19:19:01 +0000

    encode string

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -7,14 +7,18 @@
 char *rot13(char *r)
 {
 	int x, y;
-	char arr[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char arr2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	char arr[] = "abcdefghijklmnopqrstuvABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char arr2[] = "nopqrstuvwkyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (x = 0; r[x] != '\0'; x++)
 		for (y = 0; arr[y] != '\0'; y++)
 		{
 			if (r[x] == arr[y])
+			{
 				r[x] = arr2[y];
-		}
+				break;
+			}
+       		}
 	return (r);
 }
