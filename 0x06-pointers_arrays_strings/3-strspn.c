b5503167d2266f1e4ef037bae8c15584e58051fb
Trill Ali <alishabelle98@gmail.com>
2019-03-11 07:27:45 +0000

    length of prefix

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -7,13 +7,15 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int x, y;
+	int x, y, count;
 
-	for (x = 0; s[x] != '\0'; x++)
+	for (x = 0, count = 0; s[x] != '\0'; x++)
 	{
+		if (count != x)
+			break;
 		for (y = 0; accept[y] != '\0'; y++)
 			if (s[x] == accept[y])
-				accept++;
+				count++;
 	}
-	return (x);
+	return (count);
 }
