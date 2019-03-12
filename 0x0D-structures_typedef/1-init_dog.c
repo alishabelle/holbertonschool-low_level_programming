88ce323f1312c6123a0e108e61bc527a38ffbf57
Trill Ali <alishabelle98@gmail.com>
2019-03-12 20:15:53 +0000

    initialize variable

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
