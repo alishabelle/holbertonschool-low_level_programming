194ccd6cabefee91e290e53d1fa471c4b530d3cd
Trill Ali <alishabelle98@gmail.com>
2019-03-12 17:26:49 +0000

    push it all

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOGGO
+#define DOGGO
+/**
+ * struct dog - structure
+ *@name: parameter
+ *@age: parameter
+ *@owner: parameter
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
