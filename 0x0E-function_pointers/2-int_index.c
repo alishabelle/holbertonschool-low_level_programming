ba055afaf822dd925ca31c3c788e908d66fdfe26
Trill Ali <alishabelle98@gmail.com>
2019-03-14 21:01:23 +0000

    search for int

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *
+ *
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index, x;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	for (index = 0; size > index; index++)
+	{	
+		x = cmp(array[index]);
+		if (x != 0)
+		{
+			return (index);
+		}
+	}
+	return (-1);
+}
