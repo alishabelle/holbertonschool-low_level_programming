77250b03c1e0b7107c10740ecede84e393863dcf
Trill Ali <alishabelle98@gmail.com>
2019-03-18 20:20:05 +0000

    push everything

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -2,21 +2,34 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- *
- *
- *
+ *funct_c - function that prints chars
+ *@print: parameter
+ * Return: void
  */
 void funct_c(va_list print)
 {
 	printf("%c", va_arg(print, int));
 }
+
+/**
+ *funct_i - function that prints ints
+ *@print: parameter
+ * Return: void
+ */
 void funct_i(va_list print)
 {
 	printf("%d", va_arg(print, int));
 }
+
+/**
+ *funct_s - the function that prints strings
+ *@print: parameter
+ * Return: void
+ */
 void funct_s(va_list print)
 {
 	char *str = va_arg(print, char *);
+
 	if (str == NULL)
 		{
 			printf("(nil)");
@@ -24,22 +37,33 @@ void funct_s(va_list print)
 		}
 	printf("%s", str);
 }
+
+/**
+ *funct_f - function that prints floats
+ *@print: parameter
+ * Return: void
+ */
 void funct_f(va_list print)
 {
 	printf("%f", va_arg(print, double));
 }
 
+/**
+ *print_all - the function
+ *@format: parameter
+ * Return: void
+ */
 void print_all(const char * const format, ...)
 {
 	struct funct arr[] = {
 		{'c', funct_c},
 		{'i', funct_i},
 		{'s', funct_s},
-		{'f', funct_f},		
+		{'f', funct_f},
 		{'\0', NULL}
 	};
 
-       	va_list print;
+	va_list print;
 	int x = 0, y;
 	char *space = "", *sep = ", ";
 
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -5,6 +5,12 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ *struct funct - function type
+ *@s: type
+ *@f: function pointer
+ */
+
 typedef struct funct
 {
 	char s;
