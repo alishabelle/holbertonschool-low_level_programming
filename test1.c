07b576afe55a21cf3fa927eccd670d3aadfd234a
Trill Ali <alishabelle98@gmail.com>
2019-03-19 20:01:19 +0000

    test 1

---
+++ test1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+
+int main(void) {
+
+	// _printf("Character:[%c]\n", 'H');
+	// printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	_printf("Percent:[%%]\n");
+	printf("Percent:[%%]\n");
+
+	return 0;
+}
