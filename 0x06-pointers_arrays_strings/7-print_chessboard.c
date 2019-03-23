d9088ea9ced536e0a9377a147cda862ddc17d3bf
Trill Ali <alishabelle98@gmail.com>
2019-03-23 13:27:34 +0000

    print chessboard

--- 0x06-pointers_arrays_strings/7-print_chessboard.c
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -7,13 +7,13 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int col;
-	int row;
+	int row = 0;/*(x)*/
+        int col = 0;/*(y)*/
 
 	for (; col < 8; col++)
 	{
 		for (; row < 8; row++)
-			_putchar((*a + col))[row]);
-	_putchar('\n');
+			_putchar(a[col][row]);
+		_putchar('\n');
 	}
 }
