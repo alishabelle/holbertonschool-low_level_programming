c1ddd27aaf4b5b1b46b083dbf1012e9ef4990073
Trill Ali <alishabelle98@gmail.com>
2019-03-26 06:22:18 +0000

    print elements in linked list

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- *print_listint - the main function
+ *print_listint - the main function that prints elements
  *@h: parameter
- * Return - the return is a size_t type
+ * Return: the return is a size_t type
  */
 size_t print_listint(const listint_t *h)
 {
