c30db551d663f96b3cc074c248e2eb22361edcca
Trill Ali <alishabelle98@gmail.com>
2019-03-28 21:52:35 +0000

    push all

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *binary_to_uint - the function to convert binary number
+ *@b: parameter
+ * Return: the return is an unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int chan = 0;
+
+
+	while (*b != '\0')
+	{
+       		chan = 2 * chan + (*b++ - '0');
+       	}
+	return chan;
+}
---
+++ 0x13-bit_manipulation/holberton.h
@@ -0,0 +1,6 @@
+#ifndef janky
+#define janky
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+#endif
