24f129e1ad3add7ae72a586b02770e05669f1342
Trill Ali <alishabelle98@gmail.com>
2019-03-29 03:32:08 +0000

    add file

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -7,15 +7,15 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int x, sum1, sum2;
+	int x, sum1 = 0, sum2 = 0;
 
-	for (x = 0; x < size * size; x+=(size + 1))
+	for (x = 0; x < size * size; x += (size + 1))
 	{
 		sum1 = sum1 + a[x];
 	}
-	for (x = size - 1; x < (size * size) - (size - 1); x+=(size - 1))
+	for (x = size - 1; x < (size * size) - (size - 1); x += (size - 1))
 	{
 		sum2 = sum2 + a[x];
 	}
-	printf("%d, %d\n", sum1, sum2); 
+	printf("%d, %d\n", sum1, sum2);
 }
