2a1776266a298d3c0a51ad11b0b4de8e5d53e3bc
Trill Ali <alishabelle98@gmail.com>
2019-03-29 17:48:36 +0000

    print binary number

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *
+ *
+ */
+int set_bit(unsigned long int *n, usigned int index)
+{
+	unsigned long int set;
+
+	if (index > (sizeof(unsigned lont int) * 8 - 1))
+		return (-1);
+
+}
