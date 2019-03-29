378d927fc654de08be85532944bf6a4b5ca1cfc7
Trill Ali <alishabelle98@gmail.com>
2019-03-29 02:45:14 +0000

    push

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -7,4 +7,23 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int jank[1024];
+
+	int x = 0;
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+	while (n > 0)
+	{
+		jank[x] = n % 2;
+		n = n / 2;
+		x++;
+	}
+	if (jank[index] == 1)
+	{
+		return (1);
+	}
+	return (0);
 }
