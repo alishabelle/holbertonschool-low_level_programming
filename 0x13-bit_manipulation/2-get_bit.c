9cb284f8114f3bd0c1424bb99f16faf7cd5429db
Trill Ali <alishabelle98@gmail.com>
2019-03-29 01:50:25 +0000

    create file

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ *get_bit - the function to return a bit at index
+ *@n: parameter
+ *@index: parameter
+ * Return: returns an int
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+}
