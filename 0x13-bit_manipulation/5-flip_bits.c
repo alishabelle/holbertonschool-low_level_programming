29fa1a540375332745c8d8055901c8c13f853b83
Trill Ali <alishabelle98@gmail.com>
2019-03-29 01:54:02 +0000

    push it all

---
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ *flip_bits - the function to flip bits
+ *@n: parameter
+ *@m: parameter
+ * Return: return unsigned int
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+}
