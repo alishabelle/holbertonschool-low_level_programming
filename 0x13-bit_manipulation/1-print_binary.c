213ebd3d2c6b903c85d5465acb1ccab4a87b887e
Trill Ali <alishabelle98@gmail.com>
2019-03-29 01:47:20 +0000

    push file

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ *print_binary - the function to print binary
+ *@n: parameter
+ * Return: the return is void
  */
 void print_binary(unsigned long int n)
 {
