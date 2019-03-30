91dd4940bc26cd49164639964b5411e0eef0bec7
Trill Ali <alishabelle98@gmail.com>
2019-03-30 02:34:33 +0000

    flip bits

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -7,4 +7,16 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned int index;
+	unsigned long int num;
+
+	num = n ^ m;
+	index = 0;
+	while (num > 0)
+	{
+		if ((num & 1) != 0)
+			index++;
+		num = num >> 1;
+	}
+	return (index);
 }
