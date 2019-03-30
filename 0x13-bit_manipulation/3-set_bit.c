c5987dbe7508484ea0173881e7281a5232fd5bce
Trill Ali <alishabelle98@gmail.com>
2019-03-30 01:37:07 +0000

    set value at given index

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -1,13 +1,18 @@
 #include "holberton.h"
 /**
- *
- *
+ *set_bit - function to set bits
+ *@n: parameter
+ *@index: parameter
+ * Return
  */
-int set_bit(unsigned long int *n, usigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int set;
 
-	if (index > (sizeof(unsigned lont int) * 8 - 1))
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-
+	set = 1 << index;
+	*n = *n | set;
+	return (1);
 }
+
