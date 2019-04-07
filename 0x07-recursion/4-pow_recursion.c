a0a6cf77b1cd693941a01f145f60998f554c98f5
Trill Ali <alishabelle98@gmail.com>
2019-04-07 15:54:28 +0000

    function returns x raised to y

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -11,11 +11,11 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 		return (-1);
 	if (y == 0)
-		return 1;
+		return (1);
 	if (y % 2 == 0)
 	{
-		return (_pow_recursion (x , y/2) * _pow_recursion(x, y/2));
+		return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
 	}
 		else
-			return (x * _pow_recursion(x, y/2) * _pow_recursion(x, y/2));   
+			return (x * _pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
 }
