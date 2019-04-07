c5ce7e4b9de51db13f3c18dbfa88eb3be752708a
Trill Ali <alishabelle98@gmail.com>
2019-04-07 15:14:37 +0000

    function returns factorial number

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *factorial - the main function
+ *@n: parameter
+ * Return: the return is an int
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
