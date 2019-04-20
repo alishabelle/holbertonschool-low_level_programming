44fe65930e919ab5f3ccf94d05c05686539a5685
Trill Ali <alishabelle98@gmail.com>
2019-04-20 03:16:32 +0000

    push file after better

--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
