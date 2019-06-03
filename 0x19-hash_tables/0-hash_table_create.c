415aaeaa29ee2b25104b82d7ce122afe50456c4b
Trill Ali <alishabelle98@gmail.com>
2019-06-03 07:27:50 +0000

    push all files

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - a function that creates hash table
+ *
+ * Return: returns a pointer to newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+}
