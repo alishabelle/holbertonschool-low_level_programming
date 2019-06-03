1e4f3e72c38693e34d93cfe72820b5676fbca7de
Trill Ali <alishabelle98@gmail.com>
2019-06-03 10:13:48 +0000

    push answer file

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -1,11 +1,17 @@
 #include "hash_tables.h"
 
 /**
- * key_index -
- *
- * Return: 
+ * key_index - function that gives you the index of a key
+ *@key: parameter
+ *@size: size of array
+ * Return: returns index of key (int)
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	unsigned long int hasher = 0;
+	int x;
 
+	for (x = 0; key != NULL && key[x] != '\0'; x++)
+		hasher = (hasher + key[x]) % size;
+	return (hasher);
 }
