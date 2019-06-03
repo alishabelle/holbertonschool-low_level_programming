32b7fcde331ba30a2aea5045b9a9167c9d46428c
Trill Ali <alishabelle98@gmail.com>
2019-06-03 09:22:53 +0000

    push answer file

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -2,10 +2,23 @@
 
 /**
  * hash_table_create - a function that creates hash table
- *
+ *@size: size of the array
  * Return: returns a pointer to newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *new;
 
+	new = malloc(sizeof(hash_table_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->array = malloc(sizeof(hash_node_t) * size);
+
+	if (new->array == NULL)
+		return (NULL);
+
+	new->size = size;
+	return (new);
 }
