a705cf33cb54a0c31d24642a84ed7da228a8314c
Trill Ali <alishabelle98@gmail.com>
2019-06-04 01:21:27 +0000

    push answer file

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -1,11 +1,28 @@
 #include "hash_tables.h"
-
+#include <string.h>
 /**
- * hash_table_get -
- *
- * Return: 
+ * hash_table_get - retrieve a value associated with a key
+ *@ht: parameter
+ *@key: parameter
+ * Return: value associated with key or null if none
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *new_jawn;
+	unsigned long int indexer;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	indexer = key_index((unsigned char *)key, ht->size);
+
+	new_jawn = ht->array[indexer];
+
+	while(new_jawn)
+	{
+		if (strcmp(new_jawn->key, key) == 0)
+			return (new_jawn->value);
+		new_jawn = new_jawn->next;
+	}
+	return (NULL);
 }
