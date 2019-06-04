334bf3c4ea4aeff139ce53520f24e82941f3640e
Trill Ali <alishabelle98@gmail.com>
2019-06-04 03:04:22 +0000

    push output

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -1,11 +1,27 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete -
- *
- * Return: 
+ * hash_table_delete - function to delete hash table
+ *@ht: parameter
+ * Return: returns nothing
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i;
 
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i] != NULL)
+		{
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = ht->array[i]->next;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
