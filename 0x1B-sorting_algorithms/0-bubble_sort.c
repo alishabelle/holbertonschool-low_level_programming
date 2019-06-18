3eb46116e5d0b825e413f4c4fd8a095cf3eaa2b7
Trill Ali <alishabelle98@gmail.com>
2019-06-18 09:56:58 +0000

    push all files to right repo

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - fas
+ *@array: afa
+ *@size: afa
+ * Return: fdsfs
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	int i = 0, len, j = 0;
+
+	len  = size - 1;
+
+	while (j < len)
+	{
+		while (i < len - j)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = temp;
+				print_array(array, size);
+			}
+			i++;
+		}
+		i = 0;
+		j++;
+	}
+}
