1adb9bbabd4c5090ef4b85d4dcf5d340120632c4
Trill Ali <alishabelle98@gmail.com>
2019-06-23 05:52:58 +0000

    push answer file

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -1,11 +1,63 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list -
- *
- * Return: 
+ *swapper - function that swaps nodes
+ *@node: parameter
+ * Return: retruns void
+ */
+void swapper(listint_t *node)
+{
+	listint_t *uno, *dos, *sig;
+
+
+	if (node->prev == NULL)
+		return;
+	uno = node->prev;
+	dos = uno->prev;
+	sig = node->next;
+	if (dos != NULL)
+		dos->next = node;
+	node->prev = dos;
+	node->next = uno;
+	uno->prev = node;
+	uno->next = sig;
+	if (sig != NULL)
+		sig->prev = uno;
+}
+
+/**
+ * insertion_sort_list - algo for insertion sort
+ *@list: parameter
+ * Return: returns void
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *jawn, *store;
 
+	if (*list == NULL || list == NULL)
+		return;
+
+	jawn = (*list)->next;
+	while (jawn != NULL)
+	{
+		if (jawn->prev->n > jawn->n)
+		{
+			store = jawn->next;
+			while (jawn->prev != NULL)
+			{
+				if (jawn->n < jawn->prev->n)
+				{
+					swapper(jawn);
+					while ((*list)->prev != NULL)
+						(*list) = (*list)->prev;
+					print_list(*list);
+				}
+				else
+					break;
+			}
+			jawn = store;
+		}
+		else
+			jawn = jawn->next;
+	}
 }
