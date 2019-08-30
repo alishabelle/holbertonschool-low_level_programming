42a6a9e6244786e9db875878398c818c2ee0da01
alishabelle <alishabelle98@gmail.com>
2019-08-30 04:07:35 +0000

    push this

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,10 +1,26 @@
 #include "search_algos.h"
 /**
- *@array - parameter
- *@size - parameter
- *@value - parameter
+ * linear_search - some algo
+ *@array: parameter
+ *@size: parameter
+ *@value: parameter
  * Return: returns an int
  */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t a;
+
+	if (array == NULL)
+		return (-1);
+
+	for (a = 0; a < size; a++)
+	{
+		if (array[a] == value)
+		{
+			printf("Value checked array[%u] = [%u]\n", a, array[a]);
+			return (a);
+		}
+		printf("Value checked array[%u] = [%u]\n", a, array[a]);
+	}
+	return (-1);
 }
