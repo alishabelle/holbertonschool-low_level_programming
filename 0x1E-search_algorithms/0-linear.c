a7b3bce036cb82a6264abdf3d83e4fa645ddac00
alishabelle <alishabelle98@gmail.com>
2019-08-30 04:03:36 +0000

    push file

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,10 @@
+#include "search_algos.h"
+/**
+ *@array - parameter
+ *@size - parameter
+ *@value - parameter
+ * Return: returns an int
+ */
+int linear_search(int *array, size_t size, int value)
+{
+}
